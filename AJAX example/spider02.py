56d1f187f26611c5b937a53bf094eef5e5b04b7b
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-07-06 21:02:37 +0100

    spider02
    Start Parsing Response for Page 1
    Get first 8 links.
    No pagination handling yet.

---
+++ AJAX example/spider02.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+import os
+import json
+from pprint import pprint
+import urllib.parse
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.loader import ItemLoader
+
+class foodcom(scrapy.Spider):
+
+    name = 'food-com'
+    custom_settings = {"FEEDS": {"foodresults.csv":{"format":"csv"}}}
+
+    page = 1
+    start_urls = ['https://www.food.com/recipe/all/healthy?pn=' + str(page)]
+
+    headers = {
+                'user-agent' : "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
+               }
+    try:
+        os.remove("foodresults.csv")
+    except OSError:
+        pass
+
+
+
+    def parse(self,response):
+        # iterate through each batch of recipes - 8 on very first 'page'
+        res = response.xpath('//script[@type="application/ld+json"]/text()').get()
+        res = json.loads(res)
+        for i in range (8):
+            link = (res['itemListElement'][i]['url'])
+            print(link)
+
+        # add next_page code here
+        # next_page =
+
+    def parse_details(self,response):
+        pass
+        # get recipe and ingredients and yield to items / send to FEEDS
+
+# main driver #
+if __name__ == "__main__":
+    process = CrawlerProcess()
+    process.crawl(foodcom)
+    process.start()
