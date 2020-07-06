ece60dc9ccf51e35b6d2be43da24380868ced662
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-07-06 21:08:44 +0100

    Create items.py

---
+++ AJAX example/items.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+import scrapy
+
+
+class FoodcomItem(scrapy.Item):
+    # define the fields for your item here like:
+    link = scrapy.Field()
