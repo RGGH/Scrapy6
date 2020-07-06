68e95e759516c371a6eea643dad1bd1fcbcfa812
Red and Green <62441426+RGGH@users.noreply.github.com>
2020-07-06 21:49:57 +0100

    Base Spider
    Runs with no errors
    (Doesn't do anything either!)

--- AJAX example/spider01.py
+++ AJAX example/spider01.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+#|r|e|d|a|n|d|g|r|e|e|n|.|c|o|.|u|k|
+#+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
 import os
 import scrapy
 from scrapy.crawler import CrawlerProcess
